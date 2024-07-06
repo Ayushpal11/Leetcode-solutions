05dc73714fc30682127db4686ed5002606e40660
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-07-06 11:36:55 +0530

    Time: 0 ms (100%), Space: 42.3 MB (35.66%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // double ans = 1.0;   
+        // if(n<0){
+        //     n = -n;
+        //     x=1/x;
+        // }   
+        // for(int i = 0 ; i<n; i++){
+        //     ans = ans * x;
+        // }
+        // return ans;
+        return Math.pow(x,n);
+    }
+}
