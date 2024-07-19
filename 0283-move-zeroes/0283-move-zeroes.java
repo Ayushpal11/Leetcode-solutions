238fd42ff30112985b2c13cc8c7592a25805b05d
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-07-20 00:02:20 +0530

    Time: 2 ms (82.66%), Space: 46 MB (54.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,46 @@
-class Solution {
-    public void moveZeroes(int[] nums) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] != 0) temp.add(nums[i]);
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         ArrayList<Integer> temp = new ArrayList<>();
+//         for(int i = 0; i<nums.length; i++){
+//             if(nums[i] != 0) temp.add(nums[i]);
+//         }
+
+//         int nz = temp.size();
+        
+//         for (int i = 0 ; i<nz; i++){
+//             nums[i] = temp.get(i);
+//         }for (int i = nz; i< nums.length; i++){
+//             nums[i]=0;
+//         }
+//         return;
+//     }
+// }
+// O(2*N)
+// O(N)
+
+class Solution{
+    public void moveZeroes(int [] nums){
+        int j = -1;
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
         }
 
-        int nz = temp.size();
+        if (j == -1) return;
         
-        for (int i = 0 ; i<nz; i++){
-            nums[i] = temp.get(i);
-        }for (int i = nz; i< nums.length; i++){
-            nums[i]=0;
+        for (int i = j+1; i<nums.length; i++){
+            if(nums[i]!= 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
         }
         return;
     }
 }
+
+//TC O(N)
+//SC O(1)
