af6695fe5b7c77bfc66b7b665c787d30feee8949
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-07-20 15:18:48 +0530

    Time: 0 ms (100%), Space: 45 MB (76.29%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int N = nums.length;
+      int sum = (N * (N + 1)) / 2;
+      int s2 = 0;
+      for (int i = 0 ; i< N ; i++){
+        s2 += nums[i];
+      }
+      int missingNum = sum -s2;
+      return missingNum;
+    }
+}
+
+
+//Brute
+        // for (int i=1 ; i<nums.length; i++){
+        //     int flag = 0;
+        //     for (int j = 0; j<nums.length - 1; j++){
+        //         if(nums[j] == i){
+        //             flag = 1;
+        //             break;
+        //         }
+        //     }
+        //     if (flag == 0) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+// Array
+
+    //      int [] hash = new int[nums.length+1];
+    //     for(int i = 0; i<nums.length ; i++){
+    //         hash[nums[i]]++;
+    //     }
+    //     for (int i = 1; i<nums.length ; i++){
+    //         if (hash[i] == 0){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
