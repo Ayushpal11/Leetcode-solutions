635d3f168c1dab0ca2b67c82691d23aac044f00b
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-07-21 23:11:24 +0530

    Time: 1 ms (99.89%), Space: 46.5 MB (7.81%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int i=0; i<nums.length; i++){
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }   
+}
+
+// Brute
+// for (int i = 1 ; i<nums.length; i++){
+// int num = nums[i];
+// int cnt = 0;
+// for (int j = 0 ; j<nums.length; j++){
+// if (nums[j] == num){
+// cnt++;
+// }
+// }
+// if (cnt == 1) return num;
+// }
+// return 1;
+// }
+//O(N^2)
+//O(1)
