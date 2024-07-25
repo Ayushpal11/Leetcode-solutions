56ca6a0391d18e3f1cd7a8557068cc6095ada0b1
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-07-26 00:03:42 +0530

    Time: 741 ms (10.69%), Space: 56.3 MB (39.54%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -2,6 +2,8 @@ class Solution {
     public int[] sortArray(int[] essy) {
         Arrays.sort(essy);
 
+        System.out.println("Sorted Array is:");
+
         for (int x : essy){
             System.out.print(x + " ");
         }
