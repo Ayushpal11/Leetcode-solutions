ab32b931261b391a8774d14cc300e6bf053c46d1
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-08-03 07:01:09 +0530

    Time: 3 ms (85.96%), Space: 43.9 MB (92.78%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -12,15 +12,11 @@ class Solution {
         // int res = 0;
         // for (int i = 0 ; i<target.length; i++){
         //     sum += target[i];
-        // }
-        // for (int i = 0; i<arr.length; i++){
         //     res += arr[i];
         // }
-        // if (sum == res){
-        //     return true;
+        // if (sum != res){
+        //     return false;
         // }
-        // return false;
-
         return true;
     }
 }
