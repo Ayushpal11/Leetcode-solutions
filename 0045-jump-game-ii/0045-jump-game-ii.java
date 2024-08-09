91f2b0f5038384eeb2d3b16dd25ced4bf38dc82e
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-08-09 23:29:38 +0530

    Time: 1 ms (97.83%), Space: 44.9 MB (58.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int jump(int[] nums) {
         if (nums.length == 1) return 0;
-        int jump = 0, end = 0, far = 0;
+        int cnt = 0, end = 0, far = 0;
         for (int i = 0; i< nums.length; i++){
             far = Math.max(far, i + nums[i]);
             if (i == end){
-                jump++;
+                cnt++;
                 end = far;
 
                 if (end >= nums.length - 1){
@@ -14,6 +14,6 @@ class Solution {
             }
         }
 
-        return jump;
+        return cnt;
     }
 }
