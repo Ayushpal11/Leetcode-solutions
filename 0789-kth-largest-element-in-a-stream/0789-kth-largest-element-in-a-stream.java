e9f852e4d11af061dcdcec8ba807ffa6716b76c2
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-08-12 07:27:44 +0530

    Time: 13 ms (82.29%), Space: 47 MB (33.32%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -1,40 +1,50 @@
 class KthLargest {
 
-    List<Integer> arr;
-    int k;
+    // List<Integer> arr;
+    // int k;
+    private PriorityQueue<Integer> minHeap;
+    private int k;
 
     public KthLargest(int k, int[] nums) {
-        arr = new ArrayList<Integer>(nums.length);
+        // arr = new ArrayList<Integer>(nums.length);
+        minHeap = new PriorityQueue<>();
         this.k = k;
 
         for (int num: nums){
-            arr.add(num);
+            add(num);
         }
-
-        Collections.sort(arr);
+        // Collections.sort(arr);
     }
 
-    private int getIndex(int val){
-        int left = 0;
-        int right = arr.size() - 1;
-        while (left <= right){
-            int mid = (left + right) / 2;
-            int midEl = arr.get(mid);
-            if (midEl == val) return mid;
-            if (midEl > val){
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
+    // private int getIndex(int val){
+        // int left = 0;
+        // int right = arr.size() - 1;
+        // while (left <= right){
+        //     int mid = (left + right) / 2;
+        //     int midEl = arr.get(mid);
+        //     if (midEl == val) return mid;
+        //     if (midEl > val){
+        //         right = mid - 1;
+        //     }
+        //     else{
+        //         left = mid + 1;
+        //     }
+        // }
+        // return left;
+    // }
     
     public int add(int val) {
-        int index = getIndex(val);
-        arr.add(index,val);
-        return arr.get(arr.size() - k);
+        if (minHeap.size() < k){
+            minHeap.offer(val);
+        }else if (val > minHeap.peek()){
+            minHeap.poll();
+            minHeap.offer(val);
+        }
+        return minHeap.peek();
+    //     int index = getIndex(val);
+    //     arr.add(index,val);
+    //     return arr.get(arr.size() - k);
+
     }
 }
 
