7dc62686f59bd106d17881b939bc7df583f164dd
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-08-18 22:48:57 +0530

    Time: 5 ms (79.17%), Space: 61.8 MB (54.17%) - LeetHub

--- 0814-smallest-rotation-with-highest-score/0814-smallest-rotation-with-highest-score.java
+++ 0814-smallest-rotation-with-highest-score/0814-smallest-rotation-with-highest-score.java
@@ -1,26 +1,33 @@
 class Solution {
     public int bestRotation(int[] nums) {
         int n = nums.length;
-        int[] cnt = new int[2 * n];
-        int max = 0;
+        int[] cnt = new int[n];
         int sol = 0;
-        for(int  right= 0, left = 0, sum = 0; right < 2 * n ; right++){
-            int temp = right - nums[right % n];
-            if (temp >= 0){
-                cnt[Math.min(2 * n - 1, temp)]++;
-                if (temp >= left){
-                    sum++;
-                }
-            }
-            if (right - left == n - 1){
-                if (sum > max){
-                    max = sum;
-                    sol = left;
-                }
-                sum -= cnt[left];
-                left++;
-            }
+        for (int i = 0;i <n; i++){
+            cnt[(i - nums[i] + 1 + n) % n]--;
         }
+        for(int i =1; i<n; i++){
+            cnt[i] += cnt[i-1] + 1;
+            sol = cnt[i] > cnt[sol] ? i : sol;
+        }
+        // int max = 0;
+        // for(int  right= 0, left = 0, sum = 0; right < 2 * n ; right++){
+        //     int temp = right - nums[right % n];
+        //     if (temp >= 0){
+        //         cnt[Math.min(2 * n - 1, temp)]++;
+        //         if (temp >= left){
+        //             sum++;
+        //         }
+        //     }
+        //     if (right - left == n - 1){
+        //         if (sum > max){
+        //             max = sum;
+        //             sol = left;
+        //         }
+        //         sum -= cnt[left];
+        //         left++;
+        //     }
+        // }
         return sol;
     }
 }
