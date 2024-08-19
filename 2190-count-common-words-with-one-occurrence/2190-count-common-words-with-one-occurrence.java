3a4cf39560bc3a2729d695539fb7f78d32ce1d9f
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-08-19 23:52:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        int cnt = 0;
+
+        Map<String, Integer> mpp1 = new HashMap<String, Integer>();
+        for (String s:words1){
+            mpp1.put(s,mpp1.getOrDefault(s,0) + 1);
+        } 
+        Map<String, Integer> mpp2 = new HashMap<String, Integer>();
+        for (String s:words2){
+            mpp2.put(s,mpp2.getOrDefault(s,0) + 1);
+        } 
+
+        for (String s: words2){
+            if (mpp1.containsKey(s) && (mpp1.get(s)==1) && mpp2.ge(ts) == 1){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
