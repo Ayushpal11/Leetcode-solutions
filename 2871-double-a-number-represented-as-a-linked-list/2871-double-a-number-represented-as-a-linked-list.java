ad3fbd5bbf2a02d440d1ac0f2706af91453fb32c
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-08-23 09:18:48 +0530

    Time: 2 ms (99.44%), Space: 47 MB (17.15%) - LeetHub

--- 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.java
@@ -10,39 +10,49 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        ListNode revList = reverseList(head);
-        int cry = 0;
-        ListNode curr = revList, prev = null;
-
-        while (curr != null){
-            int newVal = curr.val * 2 + cry;
-            curr.val = newVal % 10;
-
-            if (newVal > 9){
-                cry = 1;
-            }else {
-                cry = 0;
+        if (head.val  > 4){
+            head = new ListNode(0,head);
+        }
+        for (ListNode node = head; node != null; node = node.next){
+            node.val = (node.val * 2) % 10;
+            if(node.next != null && node.next.val > 4){
+                node.val++;
             }
-            prev = curr;
-            curr = curr.next;
         }
-        if (cry != 0){
-            ListNode extra  = new ListNode(cry);
-            prev.next = extra;
-        }
+        return head;
+    //     ListNode revList = reverseList(head);
+    //     int cry = 0;
+    //     ListNode curr = revList, prev = null;
 
-        ListNode result = reverseList(revList);
-        return result;
-    }
-    public ListNode reverseList(ListNode node) {
-        ListNode prev = null, curr = node, nextNode;
-        while (curr != null) {
-            nextNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
+    //     while (curr != null){
+    //         int newVal = curr.val * 2 + cry;
+    //         curr.val = newVal % 10;
 
-        return prev;
+    //         if (newVal > 9){
+    //             cry = 1;
+    //         }else {
+    //             cry = 0;
+    //         }
+    //         prev = curr;
+    //         curr = curr.next;
+    //     }
+    //     if (cry != 0){
+    //         ListNode extra  = new ListNode(cry);
+    //         prev.next = extra;
+    //     }
+
+    //     ListNode result = reverseList(revList);
+    //     return result;
+    // }
+    // public ListNode reverseList(ListNode node) {
+    //     ListNode prev = null, curr = node, nextNode;
+    //     while (curr != null) {
+    //         nextNode = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = nextNode;
+    //     }
+
+    //     return prev;
     }
 }
