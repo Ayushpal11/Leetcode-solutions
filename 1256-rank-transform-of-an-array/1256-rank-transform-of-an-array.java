3e9bbd4151e23921dab5ab9f4349e677c0af31d9
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-10-02 17:20:02 +0530

    Time: 34 ms (34.63%), Space: 57.9 MB (87.77%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
         Set<Integer> set = new HashSet<>();
-        for (int num : arr){
-            set.add(num);
+        for (int a: arr){
+            set.add(a);
         }
-
-        List<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
+        List<Integer> lis = new ArrayList<>(set);
+        Collections.sort(lis);
         HashMap<Integer, Integer> mpp = new HashMap<>();
-        for (int i= 0; i<list.size(); i++){
-            mpp.put(list.get(i), i+1);
-        } 
-        for (int i=0; i < arr.length;i++){
+        for (int i=0; i<lis.size(); i++){
+            mpp.put(lis.get(i), i+1);
+        }
+        for (int i=0; i<arr.length; i++){
             arr[i] = mpp.get(arr[i]);
         }
         return arr;
