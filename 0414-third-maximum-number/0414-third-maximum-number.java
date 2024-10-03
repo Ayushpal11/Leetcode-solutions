947ee233a9dc6ce3815caac3aa226dec2784b72d
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-10-04 00:14:14 +0530

    Time: 4 ms (68.56%), Space: 42.7 MB (87.66%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,15 +1,18 @@
 class Solution {
     public int thirdMax(int[] nums) {
         Arrays.sort(nums);
-        int num = 0;
+        // Check the middle element for any changes
         for (int i=0 ; i < nums.length/2; i++){
             int temp = nums[i];
+            //  declare an temp number and replaye with the miidle element
             nums[i] = nums[nums.length - 1 - i];
             nums[nums.length - 1 - i] = temp;
         }
         int cnt = 1;
-        int prev = nums[0];
+        // Create a counter to check if we are at the 3rd Max distinct element
+        int prev = nums[0]; // prev at starting value or smallest
         for (int i=0; i< nums.length; i++){
+            //  condition to check and update prev to next values till cnt = 3
             if (nums[i] != prev){
                 cnt++;
                 prev = nums[i];
@@ -19,6 +22,7 @@ class Solution {
             }
         }
         
+        // return if length = 1.
         return nums[0];
     }
 }
