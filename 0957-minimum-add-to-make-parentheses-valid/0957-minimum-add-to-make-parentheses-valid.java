98b8a19484fd6aafdbf078c0c6bbbdce0f54e46c
Ayush Pal <91583618+Ayushpal11@users.noreply.github.com>
2024-10-09 12:53:32 +0530

    Time: 1 ms (58.43%), Space: 41.2 MB (79.36%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int cnt = 0;
+        int req = 0;
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i) == '('){
+                cnt++;
+            }else {
+                if (cnt > 0){
+                    cnt--;
+                }else {
+                    req++;
+                }
+            }
+        }
+        return Math.abs(req+cnt);
+    }
+}
